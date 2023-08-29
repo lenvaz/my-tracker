be39e488555d769f6a3ca7c2d06ad6c312885fd9
lenvaz <len.vaz@urbanasmart.com>
2023-08-30 01:35:56 +0530

    test: add test for the app component UI

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,31 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvents from "@testing-library/user-event";
 import App from "../App";
 
-describe("App", () => {
-  it("Render <App/> component successfully", () => {
+describe("Render <App/> component successfully", () => {
+  it("App counter found & rendered", async () => {
+    // Setup the user events
+    const user = userEvents.setup();
+    // Render the component
     render(<App />);
-    expect(1 + 1).toBe(2);
+    // Get the button element
+    const button = screen.getByRole<HTMLButtonElement>("button", {
+      name: /count is 0/i,
+    });
+    // click thrice to increment the count to 3
+    await user.click(button);
+    await user.click(button);
+    await user.click(button);
+    expect(button.textContent).toBe("count is 3");
+  });
+  it("App counter not found", async () => {
+    // Render the component
+    render(<App />);
+    // Find the button element and expect an Error due to no match
+    expect(
+      screen.findByRole<HTMLButtonElement>("button", {
+        name: /count is 1/i,
+      })
+    ).rejects.toThrow();
   });
 });
