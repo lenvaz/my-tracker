29757d5cff38782b7e426e48e3162850ef636904
lenvaz <len.vaz@urbanasmart.com>
2023-09-27 21:08:05 +0530

    fix: on redirect url

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const redirectUrl = `${window.location.origin}/my-tracker/`;
+console.log("redirectUrl : ", redirectUrl);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Auth0Provider
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
     clientId={import.meta.env.VITE_AUTH0_CLIENTID}
     authorizationParams={{
       // TODO: Need to write a constants file in future to hold such constants
-      redirect_uri: `${window.location.origin}/my-tracker/`,
+      redirect_uri: redirectUrl,
     }}
   >
     <React.StrictMode>
