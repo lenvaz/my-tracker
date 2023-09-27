e10e9945767e41911e602d524359cbb2823445f0
lenvaz <len.vaz@urbanasmart.com>
2023-09-27 17:45:04 +0530

    fix: redirect logout url on auth0

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const redirectUri = `${window.location.origin}${import.meta.env.BASE_URL}`;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Auth0Provider
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
     clientId={import.meta.env.VITE_AUTH0_CLIENTID}
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_uri: redirectUri,
     }}
   >
     <React.StrictMode>
