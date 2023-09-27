6a038edcf7a4c213fa6a6bed0c7e08a021376057
lenvaz <len.vaz@urbanasmart.com>
2023-09-27 21:33:02 +0530

    chore: check 2

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const redirectUrl = `${window.location.origin}/my-tracker/`;
-console.log("redirectUrl : ", redirectUrl);
 console.log("VITE_AUTH0_DOMAIN : ", import.meta.env.VITE_AUTH0_DOMAIN);
 console.log("VITE_AUTH0_CLIENTID : ", import.meta.env.VITE_AUTH0_CLIENTID);
 
@@ -15,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     clientId={import.meta.env.VITE_AUTH0_CLIENTID}
     authorizationParams={{
       // TODO: Need to write a constants file in future to hold such constants
-      redirect_uri: redirectUrl,
+      redirect_uri: `${window.location.origin}/my-tracker/`,
     }}
   >
     <React.StrictMode>
